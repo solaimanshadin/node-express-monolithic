5be7aaf1c7163b94cd30786244d85583af0992da
solaimanshadin <solaimanshadin@gmail.com>
2020-10-03 20:55:15 +0600

    [ADD] Middleware

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const morgan  = require('morgan');
 // Express app 
 
 const app = express();
@@ -9,23 +9,38 @@ app.set('view engine', 'ejs')
 // app.set('views', 'views')
 
 app.listen(9000, () => console.log("Listening for port 6000"))
+// app.use((req, res, next) => {
+//     console.log('New Request was made');
+//     console.log('Host', req.hostname);
+//     console.log('Path', req.path);
+//     next();
+// })
 
-app.get('/' , (req, res) => {
+
+//Middlewre & static file
+app.use(morgan('dev'))
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
     // res.sendFile('./views/index.html' , {root: __dirname})
     const blogs = [
-        {title: 'What a nice article', snippet: 'lorem impusm doller sit'},
-        {title: 'What a Interesting article', snippet: 'lorem impusm doller sit'},
-        {title: 'What a Awsome article', snippet: 'lorem impusm doller sit'},
+        { title: 'What a nice article', snippet: 'lorem impusm doller sit' },
+        { title: 'What a Interesting article', snippet: 'lorem impusm doller sit' },
+        { title: 'What a Awsome article', snippet: 'lorem impusm doller sit' },
     ]
-    res.render('index', {title: 'Home', blogs})
+    res.render('index', { title: 'Home', blogs })
 })
-app.get('/about' , (req, res) => {
+// app.use((req, res, next) => {
+//     console.log('In next middleware' );
+//     next();
+// })
+app.get('/about', (req, res) => {
     // res.sendFile('./views/about.html' , {root: __dirname})
-    res.render('about', {title: 'About'})
+    res.render('about', { title: 'About' })
 })
 
-app.get('/new-blog' , (req, res) => {
-    res.render('create', {title : 'Create New Blog'})
+app.get('/new-blog', (req, res) => {
+    res.render('create', { title: 'Create New Blog' })
 })
 
 app.get('/blog', (req, res) => {
@@ -33,5 +48,5 @@ app.get('/blog', (req, res) => {
 })
 
 app.use((req, res) => {
-    res.status(404).render('404' , {title: '404'})
+    res.status(404).render('404', { title: '404' })
 })
